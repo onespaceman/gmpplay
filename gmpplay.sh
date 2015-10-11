b548f9f086ff6875793af751ef423fbc8290c62d
onespaceman <onespaceman@users.noreply.github.com>
2015-10-10 21:04:07 -0400

    set artist search as default and made everything execute at the end of the script

--- gmpplay.sh
+++ gmpplay.sh
@@ -9,44 +9,47 @@ host="localhost" #GMusicProxy host
 port="9999" #GMusicProxy port
 
 #actual code
-while getopts "hcaqsr" opt; do
+#search types
+searchartist () {
+	echo "Artist?";
+	read artist;
+	search1="matches&artist=${artist// /%}";
+	search2="&num_tracks=$numtracks"; }
+
+searchalbum () {
+	echo "Album?";
+	read title;
+	search1="album&artist=${artist// /%}&title=${title// /%}"; }
+
+searchsong () {
+	echo "Song?";
+	read title;
+	search1="song&artist=${artist// /%}&title=${title// /%}"; }
+
+#option flags
+while getopts "hcasr" opt; do
   case $opt in
     h)
-	echo "-h	show option flags"
+	echo "-h	show help"
 	echo "-c	clear current playlist"
 	echo ""
+	echo "	add an artist (default)"
 	echo "-a	add an album"
-	echo "-q	add an artist"
-	echo "-s	add a song"
+	echo "-s	add a song (only creating radio stations from songs works right now due to a bug in GMP)"
 	echo "-r	create radio station from an album/artist/song"
 	exit
 	;;
     c)
-	mpc clear
+	doclear="mpc -q clear"
 	;;
     a)
-	echo "Artist?"
-	read artist
-	echo "Album?"
-	read title
-	search1="album&artist=$artist&title=$title"
-	;;
-    q)
-	echo "Artist?"
-	read artist
-	search1="artist&artist=$artist"
-	search2="&num_tracks=$numtracks"
+	option="searchalbum"
 	;;
     s)
-	echo "Artist?"
-	read artist
-	echo "Song?"
-	read title
-	search1="song&artist=$artist&song=$title"
+	option="searchsong"
 	;;
     r)
 	searchtype="get_new_station_by_search"
-	search2="&num_tracks=$numtracks"
 	;;
     \?)
 	echo "Invalid option"
@@ -54,6 +57,13 @@ while getopts "hcaqsr" opt; do
 	;;
   esac
 done
+
+#Combines options here
+searchartist
+
+$option
+
+$doclear
 
 curl -s "http://$host:$port/$searchtype?type=$search1$search2" | 
   grep -v "^#" | while read url; do mpc add "$url"; done
